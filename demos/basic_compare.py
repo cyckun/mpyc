af2e1bf5a0a7302bd72840dc8dd7ecf3e7f786a2
caoyongchao <caoyongchao@bytedance.com>
2021-04-20 11:33:33 +0800

    update compare.py
    Change-Id: I66ccc5f408d684ecddd28501ee02619e463cfc5a

--- demos/basic_compare.py
+++ demos/basic_compare.py
@@ -6,9 +6,18 @@ from mpyc.runtime import mpc
 secnum = mpc.SecInt()
 if __name__ == '__main__':
     mpc.run(mpc.start())
-    x = mpc.input(secnum(1), 0)
-    y = mpc.input(secnum(2), 1)
-    tmp = mpc.sgn(x-y)
+    x = 0
+    y = 0
+    if mpc.pid == 0:
+        print("enter your number:")
+        num_x = input('input x:')
+        x = mpc.input(secnum(num_x), 0)
+    if mpc.pid ==1:
+        num_y = input('input y:')
+        y = mpc.input(secnum(num_y), 1)
+    print("x, y = ", x, y)
+    tmp = mpc.sub(x, y)
+    tmp = mpc.sgn(tmp)
     a = mpc.run(mpc.output(tmp, 1))
     if a:
         print(f'You have received message {a}.')
